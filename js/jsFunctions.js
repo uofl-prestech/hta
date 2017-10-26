c49fd59f3ca8f99bd10ed58531e32762b45a8028
Ryan McDaniel <rcmcda01@louisville.edu>
2017-10-26 12:08:28 -0400

    physical drive additions

--- js/jsFunctions.js
+++ js/jsFunctions.js
@@ -202,12 +202,84 @@ function WMIListDrives() {
     var arKeyType = ["Unknown or other protector type", "Trusted Platform Module (TPM)", "External key", "Numerical password", "TPM And PIN", "TPM And Startup Key", "TPM And PIN And Startup Key", "Public Key", "Passphrase", "TPM Certificate", "CryptoAPI Next Generation (CNG) Protector"];
     var arDriveTypes = ["Unknown", "No Root Directory", "Removable Disk", "Local Disk", "Network Drive", "Compact Disk", "RAM"];
     // var arConversionStatus = ["Fully Decrypted", "Fully Encrypted", "Encryption In Progress", "Decryption In Progress", "Encryption Paused", "Decryption Paused"];
+    
+
+
+
+    var loc = new ActiveXObject("WbemScripting.SWbemLocator");
+    //writeToLog("Executing command: ConnectServer(\".\", \"root\\cimv2\\Security\\MicrosoftVolumeEncryption\")");
+    var svc = loc.ConnectServer(".", "root\\cimv2");
+    //writeToLog("Executing command: ExecQuery(\"SELECT * FROM Win32_EncryptableVolume\")");
+    var wmiDiskDrives = svc.ExecQuery("SELECT Caption, DeviceID FROM Win32_DiskDrive");
+    var enumDrives = new Enumerator(wmiDiskDrives);
+
+    for (; !enumDrives.atEnd(); enumDrives.moveNext()) {
+        var objDrive = enumDrives.item();
+        query = "ASSOCIATORS OF {Win32_DiskDrive.DeviceID='" + objDrive.DeviceID + "'} WHERE AssocClass = Win32_DiskDriveToDiskPartition"
+        var wmiPartitions = svc.ExecQuery(query);
+        var enumPartitions = new Enumerator(wmiPartitions);
+        
+        for (; !enumPartitions.atEnd(); enumPartitions.moveNext()) {
+            var objPartition = enumPartitions.item();
+            var wmiLogicalDisks = svc.ExecQuery("ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" + objPartition.DeviceID + "'} WHERE AssocClass = Win32_LogicalDiskToPartition");
+            var enumLogicalDisks = new Enumerator(wmiLogicalDisks);
+            
+            for (; !enumLogicalDisks.atEnd(); enumLogicalDisks.moveNext()) {
+                try{
+                    var objLogicalDisk = enumLogicalDisks.item();
+                    alert("Disk drive Caption: " + objDrive.Caption + "\nDeviceID: " + " (" + objDrive.DeviceID + ")" + "\nDrive Letter: " + objLogicalDisk.DeviceID);
+                }
+                catch(err){}
+            }
+        }
+    }
+
+
+
+
+    // ComputerName = "."
+    // Set wmiServices  = GetObject ( _
+    //     "winmgmts:{impersonationLevel=Impersonate}!//" & ComputerName)
+    // ' Get physical disk drive
+    // Set wmiDiskDrives =  wmiServices.ExecQuery ( "SELECT Caption, DeviceID FROM Win32_DiskDrive")
+    
+    // For Each wmiDiskDrive In wmiDiskDrives
+    //     WScript.Echo "Disk drive Caption: " & wmiDiskDrive.Caption & VbNewLine & "DeviceID: " & " (" & wmiDiskDrive.DeviceID & ")"
+    
+    //     'Use the disk drive device id to
+    //     ' find associated partition
+    //     query = "ASSOCIATORS OF {Win32_DiskDrive.DeviceID='" _
+    //         & wmiDiskDrive.DeviceID & "'} WHERE AssocClass = Win32_DiskDriveToDiskPartition"    
+    //     Set wmiDiskPartitions = wmiServices.ExecQuery(query)
+    
+    //     For Each wmiDiskPartition In wmiDiskPartitions
+    //         'Use partition device id to find logical disk
+    //         Set wmiLogicalDisks = wmiServices.ExecQuery _
+    //             ("ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" _
+    //              & wmiDiskPartition.DeviceID & "'} WHERE AssocClass = Win32_LogicalDiskToPartition") 
+    
+    //         For Each wmiLogicalDisk In wmiLogicalDisks
+    //             WScript.Echo "Drive letter associated" _
+    //                 & " with disk drive = " _ 
+    //                 & wmiDiskDrive.Caption _
+    //                 & wmiDiskDrive.DeviceID _
+    //                 & VbNewLine & " Partition = " _
+    //                 & wmiDiskPartition.DeviceID _
+    //                 & VbNewLine & " is " _
+    //                 & wmiLogicalDisk.DeviceID
+    //         Next      
+    //     Next
+    // Next
+
+
+
+
 
     /*  ********* Get Encryption information **********/
     try {
-        var loc = new ActiveXObject("WbemScripting.SWbemLocator");
+        loc = new ActiveXObject("WbemScripting.SWbemLocator");
         writeToLog("Executing command: ConnectServer(\".\", \"root\\cimv2\\Security\\MicrosoftVolumeEncryption\")");
-        var svc = loc.ConnectServer(".", "root\\cimv2\\Security\\MicrosoftVolumeEncryption");
+        svc = loc.ConnectServer(".", "root\\cimv2\\Security\\MicrosoftVolumeEncryption");
         writeToLog("Executing command: ExecQuery(\"SELECT * FROM Win32_EncryptableVolume\")");
         var colItems = svc.ExecQuery("SELECT * FROM Win32_EncryptableVolume");
         var enumItems = new Enumerator(colItems);
