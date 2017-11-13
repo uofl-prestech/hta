74da4336bc49f6f6c7968f2606d62b5de162befb
Ryan McDaniel <trock442@hotmail.com>
2017-11-13 10:01:02 -0500

    sysprep

---
+++ sysprep_hta.ps1
@@ -0,0 +1,6 @@
+#BitlockerInfo.ps1
+#Last Updated: 7/21/2017
+#Decription:
+#Collect encryption information and volume information for each volume on the computer
+#and combine all of the information into on ArrayList of objects for output as HTML
+#Get the ID and security principal of the current user account
